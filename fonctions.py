fd8260a228468f776c8cf2e80c45c938599efdfd
KhaledELG <elghammarti.khaled@hotmail.com>
2021-11-24 01:38:41 +0100

    feat: development of the program

--- fonctions.py
+++ fonctions.py
@@ -33,46 +33,11 @@ def GetID(Client_ID, access_token, streamer_name):
 
 #################################################################################################
 
-# Defines the function to collect the list of top games
-def GetTopGames(Client_ID, access_token):
-    URL = "https://api.twitch.tv/helix/games/top"
-    head = {
-    'Client-ID' : Client_ID,
-    'Authorization' :  "Bearer " + access_token
-    }
-    x = requests.get(URL, headers = head).json()['data']
-    return x
+#TODO : See if it is possible to have new getters
 
-def GetChannelIcalendar(Client_ID, access_token, broadcaster_id):
-    URL = "https://api.twitch.tv/helix/schedule/icalendar?broadcaster_id="+broadcaster_id
-    head = {
-    'Client-ID' : Client_ID,
-    'Authorization' :  "Bearer " + access_token
-    }
-    x = requests.get(URL, headers = head)
-    return x #return un fichier .pyc ??
-
-def GetBroadcasterSubscriptions(Client_ID, access_token, broadcaster_id):
-    URL = "https://api.twitch.tv/helix/subscriptions?broadcaster_id="+broadcaster_id
-    head = {
-    'Client-ID' : Client_ID,
-    'Authorization' :  "Bearer " + access_token
-    }
-    x = requests.get(URL, headers = head)
-    return x
-
-def GetChannelStreamSchedule(Client_ID, access_token, broadcaster_id):
-    URL = "https://api.twitch.tv/helix/schedule?broadcaster_id="+broadcaster_id
-    head = {
-    'Client-ID' : Client_ID,
-    'Authorization' :  "Bearer " + access_token
-    }
-    x = requests.get(URL, headers = head).json()
-    return x
-    #Ne trouve pas d'edt dans les 5 cas
-
-def GetVideos(Client_ID, access_token, broadcaster_id):
-    URL = "https://api.twitch.tv/helix/videos?broadcaster_id="+broadcaster_id
+# Defines the function determining the information of the first page of the JSON
+def GetFirstVideos(Client_ID, access_token, broadcaster_id):
+    URL = "https://api.twitch.tv/helix/videos?user_id="+broadcaster_id
     head = {
     'Client-ID' : Client_ID,
     'Authorization' :  "Bearer " + access_token
@@ -80,33 +45,27 @@ def GetVideos(Client_ID, access_token, broadcaster_id):
     x = requests.get(URL, headers = head).json()
     return x
 
-def GetXX(Client_ID, access_token, broadcaster_id):
-    URL = "https://api.twitch.tv/helix/streams?broadcaster_id="+broadcaster_id
+# Defines the function determining the information of the next pages of the JSON
+def GetNextVideos(Client_ID, access_token, broadcaster_id, after):
+    URL = "https://api.twitch.tv/helix/videos?user_id="+broadcaster_id+"&after="+after
     head = {
     'Client-ID' : Client_ID,
     'Authorization' :  "Bearer " + access_token
     }
     x = requests.get(URL, headers = head).json()
-    return x
-
-def GetXXX(Client_ID, access_token, broadcaster_id):
-    URL = "https://api.twitch.tv/helix/users?login="+broadcaster_id
-    head = {
-    'Client-ID' : Client_ID,
-    'Authorization' :  "Bearer " + access_token
-    }
-    x = requests.get(URL, headers = head).json()['data'][0]
     return x
 
 #################################################################################################
 
-def CreateDataSet(Client_ID, access_token, streamer_names, X):     
+#TODO : Correct this part
+
+def CreateDataSet(Client_ID, access_token, streamer_names, X,j):     
     datas = []
     for i in range (len(streamer_names)):
-        JSONContent = X(Client_ID, access_token, streamer_names[i])
-        datas.append([JSONContent['id'], JSONContent['display_name'], JSONContent['view_count']])
+        JSONContent = X(Client_ID, access_token, GetID(Client_ID, access_token, streamer_names[i]))['data'][j]
+        datas.append([JSONContent['created_at'], JSONContent['duration'], JSONContent['id'], JSONContent['title'], JSONContent['view_count']])
     dataset = pd.DataFrame(datas)
-    dataset.columns = ['Id', 'Name', 'Views']
+    dataset.columns = ['Created at', 'Duration', 'Video id', 'Title', 'Views']
     dataset.dropna(axis = 0, how = 'any', inplace = True)
     dataset.index = pd.RangeIndex(len(dataset.index))
     return dataset
--- main.py
+++ main.py
@@ -1,19 +1,49 @@
 import fonctions
 import pandas as pd
 import matplotlib.pyplot as plt
+import json
+import config
 
-
-Client_ID = "uffof989s0jlx0rsqltxfd9twmhwbs"
-streamer_name = ["ironmouse","nekomatokayu","usadapekora_hololive","akaihaato_hololive", "slipixxx"]
+# Information of the identifier and the token
+Client_ID = config.Client_ID
 access_token = fonctions.GetToken(Client_ID)
-
-df = fonctions.CreateDataSet(Client_ID, access_token, streamer_name, fonctions.GetXXX)
-print(df['Name'])
-
-##Création_Excel_data
-# writer = pd.ExcelWriter('dataset.xlsx', engine='xlsxwriter')
-# df.to_excel(writer, sheet_name='Sheet1', index=False)
-# writer.save()
+# Streamers names information
+streamer_name = ["joueur_du_grenier","slipixxx"]
+# Initialization of variables
+all_data, datas = [], []
+# Initialization of the tool for writing 
+writer = pd.ExcelWriter('dataset.xlsx', engine='xlsxwriter')
+# Start of the data collection loop
+for i in range (len(streamer_name)):
+    # Reading the first page of the JSON 
+    # Get the streamer's ID
+    broadcaster_id = fonctions.GetID(Client_ID, access_token, streamer_name[i])
+    # Getting the videos on the first page
+    JSONContent = fonctions.GetFirstVideos(Client_ID, access_token, broadcaster_id)
+    # Start of data storage in datas
+    for j in range (len(JSONContent['data'])):    
+        datas.append([JSONContent['data'][j]['created_at'], JSONContent['data'][j]['duration'], JSONContent['data'][j]['user_name'], JSONContent['data'][j]['title'], JSONContent['data'][j]['view_count']])
+    after = JSONContent['pagination']['cursor']
+    # Obtaining videos and storing data for future pages
+    stop = 0
+    while(stop <1):
+        JSONContent = fonctions.GetNextVideos(Client_ID, access_token, broadcaster_id,after)
+        for j in range (len(JSONContent['data'])):
+            datas.append([JSONContent['data'][j]['created_at'], JSONContent['data'][j]['duration'], JSONContent['data'][j]['user_name'], JSONContent['data'][j]['title'], JSONContent['data'][j]['view_count']])
+        if (len(JSONContent['pagination']) !=0):
+            after = JSONContent['pagination']['cursor']
+        else:
+            stop = 1 
+    # Data layout
+    dataset = pd.DataFrame(datas)
+    dataset.columns = ['Created at', 'Duration', 'Nom du streamer', 'Title', 'Views']
+    dataset.dropna(axis = 0, how = 'any', inplace = True)
+    dataset.index = pd.RangeIndex(len(dataset.index))
+    # Creation of the Excel file
+    dataset.to_excel(writer, sheet_name=streamer_name[i], index=False)
+    datas=[]
+writer.save()
+ 
 
 ##Création_histogramme_data
 # hist=plt.bar(df['Name'],df['Views'])
