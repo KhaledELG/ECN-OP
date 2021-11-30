1f35741b90c2ef2344117c4a22389473f7480da0
KhaledELG <elghammarti.khaled@hotmail.com>
2021-11-30 18:59:01 +0100

    feat: new features

--- fonctions.py
+++ fonctions.py
@@ -57,15 +57,70 @@ def GetNextVideos(Client_ID, access_token, broadcaster_id, after):
 
 #################################################################################################
 
-#TODO : Correct this part
+def CreateDataSet(streamer_name,datas,Client_ID, access_token, i):     
+    # Reading the first page of the JSON 
+    # Get the streamer's ID
+    broadcaster_id = GetID(Client_ID, access_token, streamer_name[i])
+    # Getting the videos on the first page
+    JSONContent = GetFirstVideos(Client_ID, access_token, broadcaster_id)
+    # Start of data storage in datas
+    for j in range (len(JSONContent['data'])):    
+        datas.append([JSONContent['data'][j]['created_at'], DurationFormat(JSONContent['data'][j]['duration']), JSONContent['data'][j]['user_name'], JSONContent['data'][j]['title'], JSONContent['data'][j]['view_count']])
+    after = JSONContent['pagination']['cursor']
+    # Obtaining videos and storing data for future pages
+    stop = 0
+    while(stop <1):
+        JSONContent = GetNextVideos(Client_ID, access_token, broadcaster_id,after)
+        for j in range (len(JSONContent['data'])):
+            datas.append([JSONContent['data'][j]['created_at'], DurationFormat(JSONContent['data'][j]['duration']), JSONContent['data'][j]['user_name'], JSONContent['data'][j]['title'], JSONContent['data'][j]['view_count']])
+        if (len(JSONContent['pagination']) !=0):
+            after = JSONContent['pagination']['cursor']
+        else:
+            stop = 1 
+    return datas
+    
+#################################################################################################
 
-def CreateDataSet(Client_ID, access_token, streamer_names, X,j):     
-    datas = []
-    for i in range (len(streamer_names)):
-        JSONContent = X(Client_ID, access_token, GetID(Client_ID, access_token, streamer_names[i]))['data'][j]
-        datas.append([JSONContent['created_at'], JSONContent['duration'], JSONContent['id'], JSONContent['title'], JSONContent['view_count']])
-    dataset = pd.DataFrame(datas)
-    dataset.columns = ['Created at', 'Duration', 'Video id', 'Title', 'Views']
-    dataset.dropna(axis = 0, how = 'any', inplace = True)
-    dataset.index = pd.RangeIndex(len(dataset.index))
-    return dataset
+def DurationFormat(str):
+    my_str=[]
+    if str.find('h')!=-1:
+        time="hms"
+    elif str.find('m')!=-1:
+        time="ms"
+    else:
+        time="s"
+    for i in range (len(time)):
+        my_str.append(str.split(time[i])[0])
+        str=str.split(time[i])[1]
+
+    if len(my_str) == 1 :
+        if len(my_str[0])>1:
+            str='00:00:'+ my_str[0]
+        else: str='00:00:0'+my_str[0]
+    elif len(my_str) == 2 :
+        if len(my_str[0])>1 and len(my_str[1])>1:
+            str= '00:' + my_str[0] + ':' + my_str[1]
+        elif len(my_str[0])>1 and len(my_str[1])<=1:
+            str= '00:' + my_str[0] + ':0' + my_str[1]
+        elif len(my_str[0])<=1 and len(my_str[1])>1:
+            str= '00:0' + my_str[0] + ':' + my_str[1]
+        else : str= '00:0' + my_str[0] + ':0' + my_str[1]
+    elif len(my_str) == 3 :
+        if len(my_str[0])>1 and len(my_str[1])>1 and len(my_str[2])>1:
+            str= my_str[0] + ':' + my_str[1] + ':' + my_str[2]
+        elif len(my_str[0])>1 and len(my_str[1])>1 and len(my_str[2])<=1:
+            str= my_str[0] + ':' + my_str[1] + ':0' + my_str[2]
+        elif len(my_str[0])>1 and len(my_str[1])<=1 and len(my_str[2])>1:
+            str= my_str[0] + ':0' + my_str[1] + ':' + my_str[2]
+        elif len(my_str[0])>1 and len(my_str[1])<=1 and len(my_str[2])<=1:
+            str= my_str[0] + ':0' + my_str[1] + ':0' + my_str[2]
+        elif len(my_str[0])<=1 and len(my_str[1])>1 and len(my_str[2])>1:
+            str= '0' + my_str[0] + ':' + my_str[1] + ':' + my_str[2]
+        elif len(my_str[0])<=1 and len(my_str[1])>1 and len(my_str[2])<=1:
+            str= '0' + my_str[0] + ':' + my_str[1] + ':0' + my_str[2]
+        elif len(my_str[0])<=1 and len(my_str[1])<=1 and len(my_str[2])>1:
+            str= '0' + my_str[0] + ':0' + my_str[1] + ':' + my_str[2]
+        else : str= '0' + my_str[0] + ':0' + my_str[1] + ':0' + my_str[2]
+    return str
+
+
--- main.py
+++ main.py
@@ -10,42 +10,34 @@ access_token = fonctions.GetToken(Client_ID)
 # Streamers names information
 streamer_name = ["joueur_du_grenier","slipixxx"]
 # Initialization of variables
-all_data, datas = [], []
+datas = []
 # Initialization of the tool for writing 
-writer = pd.ExcelWriter('dataset.xlsx', engine='xlsxwriter')
+# writer = pd.ExcelWriter('dataset.xlsx', engine='xlsxwriter')
 # Start of the data collection loop
 for i in range (len(streamer_name)):
-    # Reading the first page of the JSON 
-    # Get the streamer's ID
-    broadcaster_id = fonctions.GetID(Client_ID, access_token, streamer_name[i])
-    # Getting the videos on the first page
-    JSONContent = fonctions.GetFirstVideos(Client_ID, access_token, broadcaster_id)
-    # Start of data storage in datas
-    for j in range (len(JSONContent['data'])):    
-        datas.append([JSONContent['data'][j]['created_at'], JSONContent['data'][j]['duration'], JSONContent['data'][j]['user_name'], JSONContent['data'][j]['title'], JSONContent['data'][j]['view_count']])
-    after = JSONContent['pagination']['cursor']
-    # Obtaining videos and storing data for future pages
-    stop = 0
-    while(stop <1):
-        JSONContent = fonctions.GetNextVideos(Client_ID, access_token, broadcaster_id,after)
-        for j in range (len(JSONContent['data'])):
-            datas.append([JSONContent['data'][j]['created_at'], JSONContent['data'][j]['duration'], JSONContent['data'][j]['user_name'], JSONContent['data'][j]['title'], JSONContent['data'][j]['view_count']])
-        if (len(JSONContent['pagination']) !=0):
-            after = JSONContent['pagination']['cursor']
-        else:
-            stop = 1 
+    # Creation of dataset
+    datas = fonctions.CreateDataSet(streamer_name,datas,Client_ID, access_token, i)
     # Data layout
     dataset = pd.DataFrame(datas)
-    dataset.columns = ['Created at', 'Duration', 'Nom du streamer', 'Title', 'Views']
+    dataset.columns = ['Created at', 'Duration', 'Streamer Name', 'Title', 'Views']
     dataset.dropna(axis = 0, how = 'any', inplace = True)
     dataset.index = pd.RangeIndex(len(dataset.index))
-    # Creation of the Excel file
-    dataset.to_excel(writer, sheet_name=streamer_name[i], index=False)
-    datas=[]
-writer.save()
+#     # Creation of the Excel file
+#     dataset.to_excel(writer, sheet_name=streamer_name[i], index=False)
+#     datas=[]
+# writer.save()
  
+# Start of data processing
+# Calculation of views per streamer
+ViewsPerStreamer = dataset.groupby(['Streamer Name'])['Views'].sum()
+# Average video length per streamer
+data_tmp=dataset
+data_tmp['Duration'] = pd.to_datetime(data_tmp['Duration'])
+AverageVideoLengthPerStreamer = ((data_tmp.groupby(['Streamer Name'])['Duration']).mean()).dt.time
 
-##Création_histogramme_data
-# hist=plt.bar(df['Name'],df['Views'])
+# print(AverageVideoLengthPerStreamer)
+
+# Création_histogramme_data
+# hist=plt.bar(dataset['Streamer Name'],dataset['Views'])
 # plt.show()
-#plt.savefig("pandas_hist_01.png", bbox_inches='tight', dpi=100)
+# plt.savefig("pandas_hist_01.png", bbox_inches='tight', dpi=100)
